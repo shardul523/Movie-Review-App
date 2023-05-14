dd9aeb48d0b3c6ced9387c99df609fa8614281aa
Shardul Sisodiya <98705625+shardul523@users.noreply.github.com>
2023-05-14 15:07:37 +0530

    Completed OTP and Reset Password UI

--- movie-review-app-frontend/src/components/auth/EmailVerification.jsx
+++ movie-review-app-frontend/src/components/auth/EmailVerification.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import Container from '../Container'
 import FormTitle from '../form/FormTitle'
 import SubmitBtn from '../form/SubmitBtn'
@@ -8,13 +8,32 @@ const OTPLength = 6;
 
 export default function EmailVerification() {
   
-  const [otp, setOTP] = useState(Array.from({length: OTPLength}));
+  const [otp, setOTP] = useState(Array.from({length: OTPLength}, () => ''));
+  const [activeInputIndex, setActiveInputIndex] = useState(0);
+  const activeInput = useRef();
 
-  const handleInput = ({target}, index) => {
+  const gotoPrevField = (i) => setActiveInputIndex(Math.max(i - 1, 0));
+
+  const handleBackspace = ({key}, index) => {
+    if (otp[index]) return;
+    if (key !== 'Backspace') return;
+    gotoPrevField(index);
+  }
+
+  const handleInput = ({target}, index) => { 
+
     const {value} = target;
-    // setOTP(value);
-
+    const newOTP = [...otp];
+    newOTP[index] = value.slice(-1);
+    setOTP(newOTP);
+    
+    if (value)
+      setActiveInputIndex((prev) => Math.min(prev + 1, OTPLength - 1));
   };
+
+  useEffect(() => {
+    activeInput.current.focus();
+  }, [activeInputIndex])
 
   return (
     <div className='fixed inset-0 bg-primary -z-10 flex justify-center items-center '>
@@ -28,9 +47,10 @@ export default function EmailVerification() {
                 <div className='flex justify-between'>
                   {otp.map((val, i) => {
                     return (
-                      <input type="number" name="" id="" className='h-12 w-12 bg-transparent outline-none 
-                      border-2 border-dark-subtle rounded focus:border-white text-white text-xl text-center' 
-                      key={i} value={val || ''} onChange={(e) => handleInput(e, i)}/>
+                      <input type="number" name="" id="" onKeyDown={(e) => handleBackspace(e, i)}
+                      className='h-12 w-12 bg-transparent outline-none border-2 border-dark-subtle rounded 
+                      focus:border-white text-white text-xl text-center' required key={i} value={val} 
+                      onChange={(e) => handleInput(e, i)} ref={activeInputIndex === i ? activeInput : null}/>
                     )
                   })}
                 </div>
--- movie-review-app-frontend/src/components/auth/ResetPassword.jsx
+++ movie-review-app-frontend/src/components/auth/ResetPassword.jsx
@@ -1,5 +1,25 @@
+import Container from '../Container'
+import FormTitle from '../form/FormTitle'
+import FormInput from '../form/FormInput'
+import SubmitBtn from '../form/SubmitBtn'
+import CustomLink from '../CustomLink'
+
+
 export default function ResetPassword() {
   return (
-    <div>ResetPassword</div>
+    <div className='fixed inset-0 bg-primary -z-10 flex justify-center items-center '>
+        <Container>
+            <form action="" className='bg-secondary rounded p-6 w-96 space-y-4'>
+                <FormTitle>Please Enter Your New Password</FormTitle>
+                <FormInput name="password" placeholder="********" label="New Password" type="password"/>
+                <FormInput name="new-password" placeholder="********" label="Confirm Password" type="password"/>
+                <SubmitBtn>Submit</SubmitBtn>
+                <div className="flex justify-between">
+                    <CustomLink to="/auth/sign-in">Sign In</CustomLink>
+                    <CustomLink to="/auth/sign-up">Sign Up</CustomLink>
+                </div>
+            </form>
+        </Container>
+    </div>
   )
 }
